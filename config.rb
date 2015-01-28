d99b47c380aa3765239460b9722dd59cd68178f9
gio.liao <gio.liao@ubitus.net>
2015-01-28 10:55:55 +0800

    ggg
    Signed-off-by: gio.liao <gio.liao@ubitus.net>

---
+++ config.rb
@@ -0,0 +1,29 @@
+require 'compass/import-once/activate'
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+output_style = :expanded
+sourcemap = false
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+relative_assets = false
+sass_options = {:debug_info=>false}
+
+line_comments = false
+
+preferred_syntax = :sass
+fireapp_coffeescripts_dir = "coffeescripts"
+fireapp_coffeescript_options = {:bare=>false}
+fireapp_livescripts_dir = "livescripts"
+fireapp_livescript_options = {:bare=>false}
+fireapp_minifyjs_on_build = false
+fireapp_always_report_on_build = true
+sass_options = {:debug_info=>false}
---
+++ js/script.js
@@ -0,0 +1,65 @@
+// preload //
+
+// $(document).ready( function() {
+//     var timer;   
+//     $('body').jpreLoader({
+//         splashID: "#jSplash",
+//         loaderVPos: '0%',
+//         autoClose: true        
+//     }); 
+// });
+
+// scroll
+
+var dHeight = $(document).height();
+var wHeight = $(window).height();
+var gwH = $('.block-02').offset().top;
+
+//console.log(dHeight);
+
+$('.toGames,.toTop').click(function(){
+  $("html, body").animate( { scrollTop: $('.block-01').offset().top }, 700);
+});
+$('.toDevices').click(function(){
+  $("html, body").animate( { scrollTop: $('.block-02').offset().top }, 700);
+});
+$('.toDownload').click(function(){
+  $("html, body").animate( { scrollTop: $('.block-03').offset().top }, 700);
+});
+
+$( window ).scroll(function(){
+  var stx = $( this ).scrollTop();
+  //console.log(stx);
+  console.log( gwH );
+  $('.gw-04').css('transform', 'translate3d(0,' + stx / -0.8 + 'px,0)');
+  $('.gw-03').css('transform', 'translate3d(0,' + stx / -1.2 + 'px,0)');
+  $('.gw-02').css('transform', 'translate3d(0,' + stx / -1.5 + 'px,0)');
+  $('.gw-01').css('transform', 'translate3d(0,' + stx / -1.8 + 'px,0)');
+  $('.game-hand').css('transform', 'translate3d(0,' + stx / -30 + '%,0)');
+  if( stx >= dHeight*0.15 ) {
+    $('.blk02bg.bg02').css('transform', 'translate3d(0,' + (stx- (dHeight/6)) / -3 + 'px,0)');
+    $('.blk02bg.bg01').css('transform', 'translate3d(0,' + (stx- (dHeight/6)) / -5 + 'px,0)');
+  }
+  if( stx >= gwH ) {
+    $('.game-wall , .game-hand').attr('style', '');   
+  }
+  if( stx > gwH ) {
+    var opcr = stx / dHeight - 0.4;
+    console.log( opcr );
+    $('.block-03').css('background-color','rgba(0,0,0,' + opcr * 2.2 + ')');
+    $('.gw-04').css('transform', 'translate3d(0,' + opcr * 120 + 'px,0)');
+    $('.gw-03').css('transform', 'translate3d(0,' + opcr * 150 + 'px,0)');
+    $('.gw-02').css('transform', 'translate3d(0,' + opcr * 200 + 'px,0)');
+    $('.gw-01').css('transform', 'translate3d(0,' + opcr * 280 + 'px,0)');
+    //$('.game-hand').css('transform', 'translate3d(0,' + opcr * 20 + '%,0)')
+  }
+});
+
+$('.dvc').click(function(){
+    $('.devices').fadeIn(200);
+    $('.dvc-mask').show();
+});
+$('.devices .close, .dvc-mask').click(function(){
+    $('.devices').fadeOut(100);
+    $('.dvc-mask').hide()
+});
