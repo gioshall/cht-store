7c2aef713a696e0e834956a7664f4806cc6d6341
gio.liao <gio.liao@ubitus.net>
2015-01-30 16:06:05 +0800

    ddd

--- js/script.js
+++ js/script.js
@@ -11,6 +11,30 @@ $(document).ready( function() {
 
 // device
 
+$(document).ready(function(){
+var device = navigator.userAgent.toLowerCase();
+var mobile = device.match(/(iphone|ipod|ipad|android)/);
+if (mobile) {
+     window.blockMenuHeaderScroll = false;
+$(window).on('touchstart', function(e)
+{
+    if ($(e.target).closest('#mobileMenuHeader').length == 1)
+    {
+        blockMenuHeaderScroll = true;
+    }
+});
+$(window).on('touchend', function()
+{
+    blockMenuHeaderScroll = false;
+});
+$(window).on('touchmove', function(e)
+{
+    if (blockMenuHeaderScroll)
+    {
+        e.preventDefault();
+    }
+});
+});
 
 
 // scroll
